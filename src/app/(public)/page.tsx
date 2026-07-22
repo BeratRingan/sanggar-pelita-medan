1b7a1342c980724d45e1fd28b1f81f2f5b905340
BeratRingan <paroronajogy@gmail.com>
2026-07-22 20:01:25 +0700

    refactor: add public layout shell

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,3 @@
-import { Navbar } from "@/components/layout/navbar";
-import { Footer } from "@/components/layout/footer";
 import { HeroSection } from "@/components/home/hero-section";
 import { AboutSection } from "@/components/home/about-section";
 import { LatestArticles } from "@/components/home/latest-articles";
@@ -7,15 +5,9 @@ import { LatestArticles } from "@/components/home/latest-articles";
 export default function HomePage() {
   return (
     <>
-      <Navbar />
-
-      <main>
-        <HeroSection />
-        <AboutSection />
-        <LatestArticles />
-      </main>
-
-      <Footer />
+      <HeroSection />
+      <AboutSection />
+      <LatestArticles />
     </>
   );
 }
